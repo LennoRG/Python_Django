adc77fa7c5973bff0587974ccd20f95512c434f7
Lenno <54871866+LennoRG@users.noreply.github.com>
2020-03-16 21:52:01 -0600

    Herencia de modelos - ClaseModelos

--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True) #Todo Objeto creado
+    fc = models.TimeField(auto_now_add=True) #Fecha de creacion  o registro de Objeto
+    fm = models.TimeField(auto_now=True) #Fecha de modificacion de objeto
+    uc = models.ForeignKey(User, on_delete=models.CASCADE) #ClaseModelo mantiene una relacion de uno a muchos con User
+    um = models.IntegerField(blank=True, null=True) #Usuario modifica, se permiten valores vacios y nulos
+
+    class Meta:
+        abstract=True
