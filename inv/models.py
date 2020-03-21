568d33df2888654a0f1a9f4955bccf24bf3eb806
Lenno <54871866+LennoRG@users.noreply.github.com>
2020-03-21 13:52:04 -0600

    Categoria - Modelo

--- inv/models.py
+++ inv/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from bases.models import ClaseModelo
+
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Categoria',
+        inique=True
+    )
+
+    def __str__(self):    #Cuando se carga categoria hace referencia a la descripción 
+        return '{}'.format(self.descripcion)
+        
+    def save(self):
+        self.descripcion = self.descripcion.upper() #Pongo la descripción en mayusculas
+        super(Categoria, self).save() # Para guardarlo mando a traer el metodo save de la clase padre Categoria
+
+    class Meta:
+        verbose_name_plural = 'Categorias' #Guardo el nombre cuando se refriera a plural
