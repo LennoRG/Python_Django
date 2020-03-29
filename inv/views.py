2c6fa2512c117c2d865a959a010ddbb6bed016d2
Lenno <54871866+LennoRG@users.noreply.github.com>
2020-03-29 17:09:40 -0600

    Categoria - Edit - Vista

--- inv/views.py
+++ inv/views.py
@@ -16,12 +16,24 @@ class CategoriaView(LoginRequiredMixin, generic.ListView):
 
 class CategoriaNew(LoginRequiredMixin, generic.CreateView):
     model=Categoria
-    template_name = "inv/categoria_form.html"
-    context_object_name = "obj"
-    form_class = CategoriaForms
-    success_url = reverse_lazy("inv:categoria_list")
-    login_url = 'bases:login'
+    template_name="inv/categoria_form.html"
+    context_object_name="obj"
+    form_class=CategoriaForms
+    success_url=reverse_lazy("inv:categoria_list")
+    login_url = "bases:login"
 
     def form_valid(self, form):
         form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model=Categoria
+    template_name="inv/categoria_form.html"
+    context_object_name="obj"
+    form_class=CategoriaForms
+    success_url=reverse_lazy("inv:categoria_list")
+    login_url = "bases:login"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
         return super().form_valid(form)
