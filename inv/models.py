db0a09f54b65ab9e28ce3fcde0d666ff131b1919
Lenno <54871866+LennoRG@users.noreply.github.com>
2020-04-05 14:18:35 -0500

    SubCategoria - Modelo y SubCategoria - Migración

--- inv/models.py
+++ inv/models.py
@@ -17,4 +17,26 @@ class Categoria(ClaseModelo):
         super(Categoria, self).save() # Para guardarlo mando a traer el metodo save de la clase padre Categoria
 
     class Meta:
-        verbose_name_plural = 'Categorias' #Guardo el nombre cuando se refriera a plural
+        verbose_name_plural = "Categorias" #Guardo el nombre cuando se refriera a plural
+    
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+     max_length=100,
+     help_text='Descripción de la Categoria'
+    #unique=True
+          
+    )
+
+    def __str__(self):
+        return '{}:{}'.format(self.categoria.descripcion,self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(SubCategoria, self).save()
+
+    class Meta:
+        verbose_name_plural = "Sub Categorias"
+        unique_together = ('categoria','descripcion') #Unique compuesto
+
+
